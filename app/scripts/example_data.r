f566a6340c5f2ed7006cf29af857f4fd5c97637e
Maximilian Hahn <max.hahn@gmx.de>
2024-07-12 17:29:51 +0200

    added ci tests for comparison and started error analysis of bad conditioning set

--- app/scripts/example_data.r
+++ app/scripts/example_data.r
@@ -11,22 +11,24 @@ library(rIOD)
 n_cores <- 8
 plan("multicore", workers = n_cores, gc=TRUE)
 
-adag_out <- getDAG("pdsep_g")
-truePAG <- getTruePAG(adag_out$dagg)
-trueAdjM <- truePAG@amat
-renderAG(trueAdjM)
+
 
 # create datasets for 3 servers separately
 
 # They are saved in data[[1]], data[[2]], data[[3]]
 
-get_example_data <- function(c, n, f) {
+get_example_data <- function(dag_type, num_clients, num_samples, num_vars) {
+
+  adag_out <- getDAG(dag_type)
+  truePAG <- getTruePAG(adag_out$dagg)
+  trueAdjM <- truePAG@amat
+  renderAG(trueAdjM)
 
   data <- list()
-  for (i in 1:c) {
-    adat_out <- FCI.Utils::generateDataset(adag = adag_out$dagg, N=n, type = "continuous")
+  for (i in 1:num_clients) {
+    adat_out <- FCI.Utils::generateDataset(adag = adag_out$dagg, N=num_samples, type = "continuous")
     cur_full_dat <- adat_out$dat
-    data[[i]] <-  cur_full_dat[, sample(1:ncol(cur_full_dat), size = f)] #generated datasets
+    data[[i]] <-  cur_full_dat[, sample(1:ncol(cur_full_dat), size = num_vars)] #generated datasets
   }
   data
 }
