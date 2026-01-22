a4673573b00dd4aeb37f69a308fe4eb5715b3ebb
maxhahn <max.hahn@gmx.de>
2026-01-22 16:55:02 +0100

    enabled additive masking by default

--- fedci/env.py
+++ fedci/env.py
@@ -6,4 +6,4 @@ def get_env_line_search(): return True if (v := os.getenv("LINE_SEARCH")) is Non
 def get_env_lm_damping(): return True if (v := os.getenv("LM_DAMPING")) is None else bool(int(v))
 def get_env_fit_intercept(): return True if (v := os.getenv("FIT_INTERCEPT")) is None else bool(int(v))
 def get_env_client_heterogeniety(): return 1 if (v := os.getenv("CLIENT_HETEROGENIETY")) is None else int(v)
-def get_env_additive_masking(): return False if (v := os.getenv("ADDITIVE_MASKING")) is None else bool(int(v))
+def get_env_additive_masking(): return True if (v := os.getenv("ADDITIVE_MASKING")) is None else bool(int(v))
